91adb33dbc8eea4c2bab86418b9d6acaf88b1104
PiotrCodi <pgwoinski@gmail.com>
2024-09-14 19:20:51 +0200

    GridCell and BaseTurret changes

--- Assets/Scripts/BaseTurret.cs
+++ Assets/Scripts/BaseTurret.cs
@@ -19,13 +19,23 @@ public abstract class BaseTurret : MonoBehaviour
     [SerializeField] protected SpriteRenderer spriteRenderer;
     [SerializeField] protected TurretUpgrades upgrades;
     [SerializeField] protected GameObject upgradesMenu;
-
+    private RectTransform upgradesMenuRectTransform;
+    private Camera mainCamera;
     private bool upgradesMenuOpen = false;
+    Vector3 currentPosition;
+    private static BaseTurret currentlyOpenMenu = null;
 
+    Vector3 basePosition;
     protected virtual void Start()
     {
-       range = baseRange;
+  
+        upgradesMenuRectTransform = upgradesMenu.GetComponent<RectTransform>();
+        mainCamera = Camera.main;
+        range = baseRange;
        damage = baseDamage;
+        currentPosition = upgradesMenuRectTransform.position;
+
+        basePosition = currentPosition;
     }
 
     protected virtual void OnEnable()
@@ -76,8 +86,64 @@ public abstract class BaseTurret : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if (currentlyOpenMenu != null && currentlyOpenMenu != this)
+        {
+            currentlyOpenMenu.CloseUpgradesMenu();
+        }
+
         upgradesMenuOpen = !upgradesMenuOpen;
-        upgradesMenu.SetActive(upgradesMenuOpen);
+
+        if (upgradesMenuOpen)
+        {
+            ShowUpgradesMenu();
+            currentlyOpenMenu = this;
+        }
+        else
+        {
+            CloseUpgradesMenu();
+        }
     }
+
+
+    private void ShowUpgradesMenu()
+    {
+        upgradesMenu.SetActive(true);
+        currentPosition = basePosition;
+        currentPosition.z -=15;
+
+        if (upgradesMenuRectTransform.position.x > 0 && upgradesMenuRectTransform.position.y > 0)
+        {
+            currentPosition.x -= 2;
+            currentPosition.y -= 2;
+            upgradesMenuRectTransform.position = currentPosition;
+        }
+        else if (upgradesMenuRectTransform.position.x < 0 && upgradesMenuRectTransform.position.y > 0)
+        {
+            currentPosition.y -= 2;
+            upgradesMenuRectTransform.position = currentPosition;
+        }
+        else if (upgradesMenuRectTransform.position.x > 0 && upgradesMenuRectTransform.position.y < 0)
+        {
+            currentPosition.x -= 2;
+            upgradesMenuRectTransform.position = currentPosition;
+        }
+        else
+        {
+            upgradesMenuRectTransform.position = currentPosition;
+
+        }
+    }
+
+    private void CloseUpgradesMenu()
+    {
+        upgradesMenu.SetActive(false);
+        upgradesMenuOpen = false;
+
+        if (currentlyOpenMenu == this)
+        {
+            currentlyOpenMenu = null;
+        }
+    }
+
 
 }
