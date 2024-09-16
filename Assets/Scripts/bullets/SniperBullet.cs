77d2795220792026c238baaed01eb3e7f47a739a
Rozoom7721 <rozumpl2@gmail.com>
2024-09-16 20:16:24 +0200

    SniperTurretFirstIteration

---
+++ Assets/Scripts/bullets/SniperBullet.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperBullet : BaseBullet
+{
+	[Header("Points")]
+    [SerializeField]private Transform firingPoint;
+	[Header("ShatteredShot")]
+	[SerializeField]private GameObject bulletPrefab;
+	[SerializeField]private float fanAngle = 20f;
+	[SerializeField]private int projectileCount = 5;
+	[SerializeField]private float projectileSpeed = 5f;
+
+	protected override void OnEnemyContact(BaseEnemy enemy, DamageInfo damageInfo)
+    {
+        enemy.DealDamage(damageInfo);
+
+		GameObject bulletObj = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+		BaseBullet bullet = bulletObj.GetComponent<BaseBullet>();
+		bullet.SetDamageInfo(damageInfo);
+		ShootFan(bulletObj);
+		Destroy(gameObject);
+    }
+	public void ShootFan(GameObject bulletObj)
+	{
+		float halfAngle = fanAngle / 2f;
+
+		for (int i = 0; i < projectileCount; i++)
+		{
+			// Oblicz proporcjonalny k¹t dla ka¿dego pocisku
+			float angle = Mathf.Lerp(-halfAngle, halfAngle, (float)i / (projectileCount - 1));
+			float angleRad = angle * Mathf.Deg2Rad;
+
+			// Oblicz kierunek na podstawie k¹ta
+			Vector2 direction = new Vector2(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+
+			// Obróæ kierunek zgodnie z obrotem obiektu strzelaj¹cego
+			direction = Quaternion.Euler(0, 0, transform.eulerAngles.z) * direction;
+
+			// Utwórz pocisk
+
+
+			// Ustaw kierunek pocisku
+			Rigidbody2D rb = bulletObj.GetComponent<Rigidbody2D>();
+			rb.velocity = direction * projectileSpeed;
+		}
+	}
+}
