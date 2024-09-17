6e0cba1e9a48430bea5b1cda1f949a42c03a6b19
Rozoom7721 <rozumpl2@gmail.com>
2024-09-17 18:42:01 +0200

    First time Aura turret (U need to see this)

---
+++ Assets/Scripts/BaseAuraTurret.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class BaseAuraTurret : BaseTurret
+{
+	[Header("Layers")]
+	[SerializeField] private LayerMask enemyLayer;
+
+	protected override void HandleUpgradeEvent(BaseTurret turret)
+	{
+		if (turret != this) return; ;
+	}
+	protected override void Start()
+	{
+		base.Start();
+		StartCoroutine(UpdateEnemiesInRange());
+		Debug.Log("Started");
+	}
+
+	protected IEnumerator UpdateEnemiesInRange()
+	{
+		while (true)
+		{
+			Debug.Log("Checking for enemies");
+			Shoot();
+			yield return new WaitForSeconds(base.fireCooldown);
+		}
+	}
+	protected void Shoot()
+	{
+		Debug.Log("Shoot");
+		Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, range, enemyLayer);
+		foreach (var enemy in enemies)
+		{
+			BaseEnemy baseEnemy = enemy.GetComponent<BaseEnemy>();
+			if(baseEnemy != null)
+			{
+				Debug.Log("Enemy in range");
+				baseEnemy.DealDamage(new DamageInfo(damageType, damageSource, damage));
+			}
+		}
+	}
+	protected override void CalculateFireCooldown()
+	{
+	}
+
+
+}
--- Assets/Scripts/bullets/ShatteredBullet.cs
+++ Assets/Scripts/bullets/ShatteredBullet.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 
 public class ShatteredBullet : BaseBullet
 {
-<<<<<<< HEAD
     
     protected override void OnEnemyContact(BaseEnemy enemy, DamageInfo damageInfo)
     {
-        if (enemy.isExcluded == false)
-        {
+
            enemy.DealDamage(damageInfo);
             Destroy(gameObject);
-        }
-=======
-    protected override void OnEnemyContact(BaseEnemy enemy, DamageInfo damageInfo)
-    {
-        enemy.DealDamage(damageInfo);
-        Destroy(gameObject);
->>>>>>> origin/Create_Towerv2
+
     }
 }
