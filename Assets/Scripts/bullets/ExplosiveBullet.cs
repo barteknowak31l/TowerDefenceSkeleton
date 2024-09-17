5985fa5822a6f37e4a2c07d8355750204427d1ef
Rozoom7721 <rozumpl2@gmail.com>
2024-09-17 21:24:40 +0200

    Configure Explosive turrets

--- Assets/Scripts/bullets/ExplosiveBullet.cs
+++ Assets/Scripts/bullets/ExplosiveBullet.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class ExplosiveBullet : BaseBullet
 {
+    [Header("Explosive Stats")]
+    [SerializeField] private float explosionRadius = 1f;
+    [SerializeField] private LayerMask enemyLayer;
     protected override void OnEnemyContact(BaseEnemy enemy, DamageInfo damageInfo)
     {
-        enemy.DealDamage(damageInfo);
+        Collider2D[] hitEnemys = Physics2D.OverlapCircleAll(transform.position, explosionRadius, enemyLayer);
+
+        foreach (var hitEnemy in hitEnemys)
+        {
+            BaseEnemy baseEnemy = hitEnemy.GetComponent<BaseEnemy>();
+			if (baseEnemy != null)
+			{
+				baseEnemy.DealDamage(damageInfo);
+			}
+        }
         Destroy(gameObject);
     }
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, explosionRadius);
+	}
 }
