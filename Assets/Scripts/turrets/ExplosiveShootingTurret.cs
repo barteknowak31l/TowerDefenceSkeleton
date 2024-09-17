20fb49aba767dfa99765ae59348f1e0c086716e0
Rozoom7721 <rozumpl2@gmail.com>
2024-09-17 20:11:50 +0200

    Add Explosive turret

--- Assets/Scripts/turrets/ExplosiveShootingTurret.cs
+++ Assets/Scripts/turrets/ExplosiveShootingTurret.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FastShootingTurret : BaseShootingTurret
+public class ExplosiveShootingTurret : BaseShootingTurret
 {
 	protected override void Shoot()
 	{
