51a8a0e9f62e05e47090a3967ce09cd56e76276f
Rozoom7721 <rozumpl2@gmail.com>
2024-09-17 18:42:01 +0200

    First time Aura turret (U need to see this)

--- Assets/Scripts/bullets/SniperBullet.cs
+++ Assets/Scripts/bullets/SniperBullet.cs
@@ -14,7 +14,6 @@ public class SniperBullet : BaseBullet
 
 	protected override void OnEnemyContact(BaseEnemy enemy, DamageInfo damageInfo)
     {
-		enemy.
         enemy.DealDamage(damageInfo);
 		ShootFan();
 		Destroy(gameObject);
@@ -35,7 +34,6 @@ public class SniperBullet : BaseBullet
 			GameObject bulletObj = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
 			BaseBullet bullet = bulletObj.GetComponent<BaseBullet>();
 			bullet.SetDamageInfo(damageInfo);
-
 			Rigidbody2D rb = bulletObj.GetComponent<Rigidbody2D>();
 			rb.velocity = direction * projectileSpeed;
 		}
